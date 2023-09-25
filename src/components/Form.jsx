2817afe5610b8a6ac047f68569ccf86df27d39ee
ChukwumaKingsley <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-09-25 12:59:09 +0100

    Added meme image to page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,32 @@
+import memesData from "../memesData"
+
+import { useState } from "react"
+
 function Form() {
-    function func() {
-            console.log("I was clicked!")
-        }
+    function getRandomImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        return memesArray[randomNumber].url
+    }
+
+    const [ImageUrl, setImageURL] = useState("")
+
+    function changeImage() {
+        setImageURL(url => getRandomImage())
+    }
+    
+
     return (
-        <form className="Form">
+        <div className="Form">
             <section className="meme-text-input">
                 <input type="text" className="top-text" placeholder="Enter top meme text" />
                 <input type="text" className="bottom-text" placeholder="Enter bottom meme text" />
             </section>
-            <button className="get-image-button" onClick={func}>Get a new meme image  🖼</button>
-        </form>
+            <button className="get-image-button" onClick={changeImage}>
+                Get a new meme image  🖼
+            </button>
+            <img src={ImageUrl} className="meme-image" />
+        </div>
     )
 }
 
